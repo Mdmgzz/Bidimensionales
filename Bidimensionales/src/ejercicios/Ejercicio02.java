9b2c175b56aa0c630861a12277472cded5ed9454
migue <migue@DESKTOP-HCUS7JC>
2023-02-21 23:58:18 +0100

    repaso

--- Bidimensionales/src/ejercicios/Ejercicio02.java
+++ Bidimensionales/src/ejercicios/Ejercicio02.java
@@ -1,9 +1,30 @@
 package ejercicios;
 
+import java.util.Arrays;
+
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//bloque de variables
+				int tabla [][]= new int [11][10];
+				
+				// creamos un for para rellenar el array
+				
+				for ( int i=0; i<tabla.length;i++) {
+					
+					for (int j=0;j<10;j++) {
+						tabla[i][j]=i*j;
+						
+					}
+					System.out.println(Arrays.toString(tabla[i]));
+				}
+				for (int i=0 ; i<tabla.length;i++) {
+					for (int j=0 ; j<tabla[i].length ;j++) {
+						System.out.print(tabla[i][j] + " ");
+					}
+					System.out.println();
+					}
+			
 
 	}
 
--- Bidimensionales/src/ejercicios/Ejercicio03.java
+++ Bidimensionales/src/ejercicios/Ejercicio03.java
@@ -1,10 +1,42 @@
-package ejercicios;
+ 	package ejercicios;
+
+import java.util.Scanner;
 
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// bloque de variables
+		double tabla [][]= new double [4][5];
+		double media=0;
+		double aux=0;
+		// creamos scanner para poder introducir las notas
+		Scanner sc= new Scanner (System.in);
+		
+		
+		for ( int i=0; i<tabla.length;i++) {
+			System.out.println("Introduce las notas para el alumno: ");
+			for (int j=0;j<5;j++) {
+				System.out.println((j+1)+" asignatura");
+				tabla[i][j]=sc.nextInt();
+				
+				}
+			}
+		for (int i=0 ; i<tabla.length;i++) {
+			aux=0;
+			System.out.println("Alumno"+(i+1));
+			for (int j=0 ; j<tabla[i].length ;j++) {
+				System.out.print(tabla[i][j] + " ");
+				aux+=tabla[i][j];
+			}
+			media = aux/5;
+			System.out.println("la nota media es de : "+media);
+			System.out.println();
+		}
+		
+		
+		
+		
+		//cerramos scanner 
+		sc.close();
 	}
-
 }
